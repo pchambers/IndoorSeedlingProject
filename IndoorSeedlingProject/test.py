25f807775449ad495d0544e3d361ac630a819bbb
pchambers <peter.chambers1@gmail.com>
2018-03-24 13:29:39 -0400

    Added test for relay board functionality

--- IndoorSeedlingProject/test.py
+++ IndoorSeedlingProject/test.py
@@ -9,6 +9,7 @@ relay6 = onionGpio.OnionGpio(9)
 relay7 = onionGpio.OnionGpio(2)
 relay8 = onionGpio.OnionGpio(3)
 
+
 relay1.setOutputDirection(1)
 relay2.setOutputDirection(1)
 relay3.setOutputDirection(1)
@@ -17,7 +18,41 @@ relay5.setOutputDirection(1)
 relay6.setOutputDirection(1)
 relay7.setOutputDirection(1)
 relay8.setOutputDirection(1)
+while 1:
+	relay1.setValue(0)
+	time.sleep(2)
+	relay1.setValue(1)
 
+	relay2.setValue(0)
+	time.sleep(2)
+	relay2.setValue(1)
+
+	relay3.setValue(0)
+	time.sleep(2)
+	relay3.setValue(1)
+	
+	relay4.setValue(0)
+	time.sleep(2)
+	relay4.setValue(1)
+	
+	relay5.setValue(0)
+	time.sleep(2)
+	relay5.setValue(1)
+
+	relay6.setValue(0)
+	time.sleep(2)
+	relay6.setValue(1)
+
+	relay7.setValue(0)
+	time.sleep(2)
+	relay7.setValue(1)
+
+	relay8.setValue(0)
+	time.sleep(2)
+	relay8.setValue(1)
+
+
+"""
 relay1.setValue(1)
 relay2.setValue(1)
 relay3.setValue(1)
@@ -52,3 +87,4 @@ while 1:
 	print 'on'
 	
 	time.sleep(1)
+"""
